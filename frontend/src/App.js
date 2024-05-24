b820da9bbb0cffe7cf5639dbd43ca9b84ac535e9
Hardik Sharma <sharmahardikaz@gmail.com>
2024-05-24 23:22:38 +0530

    Frontend improved with add update functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import ToDo from "./components/ToDo"
 import {useState,useEffect} from 'react'
-import {getAllToDo} from "./utils/HandleApi"
+import {getAllToDo , addToDo} from "./utils/HandleApi"
 function App() {
 
   const [toDo, setToDo]=useState([])
-  
+  const [text, setText]=useState("");
+  const[isUpdating, setIsUpdating] =useState(false); 
+
   useEffect(() => {
     getAllToDo(setToDo)
   },[])
@@ -15,8 +17,14 @@ function App() {
      <div className="container">
       <h1>ToDo App</h1>
       <div className="top">
-      <input type="text" placeholder="Add ToDo..."/>
-      <div className="add">Add</div>
+      <input 
+      type="text" 
+      placeholder="Add ToDo..."
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      />
+
+      <div className="add" onClick={isUpdating ? () => updateToDo() : () => addToDo(text, setText, setToDo)}>{isUpdating ?"Update":"Add"}</div>
       </div>
       <div className="list">
 
--- frontend/src/utils/HandleApi.js
+++ frontend/src/utils/HandleApi.js
@@ -11,4 +11,28 @@ const getAllToDo = (setToDo) => {
     })
 }
 
-export {getAllToDo}
+const addToDo = (text, setText, setToDo) => {
+
+    axios
+    .post(`${baseUrl}/save`,{text})
+    .then((data)=>{
+        console.log(data);
+        setText("")
+        getAllToDo(setToDo)
+    })
+    .catch((err)=> console.log(err))
+}
+
+const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
+
+    axios
+    .post(`${baseUrl}/update`,{_id: toDoId,text})
+    .then((data)=>{
+        console.log(data);
+        setText("")
+        setIsUpdating(false)
+        getAllToDo(setToDo)
+    })
+    .catch((err) => console.log(err))
+}
+export {getAllToDo,addToDo, updateToDo}
